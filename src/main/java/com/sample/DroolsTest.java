b0e033eabd53900855a22a93864a797727224ce4
X279 <1419054961@qq.com>
2019-10-08 01:15:37 +0800

    软件体系结构第三章作业3

---
+++ src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,71 @@
+package com.sample;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import java.util.Scanner;
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KnowledgeBase kbase = readKnowledgeBase();
+            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+            // go !
+            Scanner in = new Scanner(System.in);
+            System.out.println("请输入需要评级的分数（输入-1退出循环）：");
+            int score = in.nextInt();
+            while(score!=-1)
+            {
+            	Score scoredata = new Score(score);
+            	ksession.insert(scoredata);
+            	ksession.fireAllRules();
+            	System.out.println("请输入需要评级的分数（输入-1退出循环）：");
+            	score = in.nextInt();
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static KnowledgeBase readKnowledgeBase() throws Exception {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(ResourceFactory.newClassPathResource("Sample.drl"), ResourceType.DRL);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }
+
+    public static class Score {
+
+    	public int score;
+    	public Score(int score) {
+			// TODO Auto-generated constructor stub
+    		this.score = score;
+		}
+    	public int getScore()
+    	{
+    		return this.score;
+    	}
+    }
+
+}
